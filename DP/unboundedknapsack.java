2e879b2ffc370516064e17e1890afc34a46f7e39
lakshita15 <lakshitamahajan2001@gmail.com>
2021-06-04 18:47:05 +0530

    unbounded knapsack

--- DP/unboundedknapsack.java
+++ DP/unboundedknapsack.java
@@ -7,29 +7,27 @@ import java.io.*;
 	    public static void unboundedknapsack(String[] args) throws Exception {
             Scanner scn = new Scanner(System.in);
             int n = scn.nextInt();
+            int weight[] = new int[n];
             int val[] = new int[n];
             for(int i=0;i<n;i++){
                 val[i] = scn.nextInt();
             }
-            int weight[] = new int[n];
             for(int i=0;i<n;i++){
                 weight[i] = scn.nextInt();
             }
             int tar = scn.nextInt();
-            System.out.println(unboundKnapsack(val,weight,tar));
+    
+            
+            int dp[] = new int[tar+1];
+            for(int i = 0; i <n; i++){ 
+            int value = val[i];
+            int wgt = weight[i];
+            for(int c = wgt ; c <= tar ; c++){
+                dp[c] = Math.max(value + dp[c-wgt] , dp[c]);
+            }
+        } 
+        System.out.println(dp[tar]) ;
 	    }
 	    
-	    public static int unboundKnapsack(int val[],int weight[],int tar){
-	        
-	        int dp[] = new int[tar+1]; 
-	        for(int i = 0; i <= tar; i++){ 
-            for(int j = 0; j < val.length; j++){ 
-                if(weight[j] <= i){ 
-                    dp[i] = Math.max(dp[i], dp[i - weight[j]] +  
-                                val[j]); 
-                } 
-            } 
-        } 
-        return dp[tar]; 
-	    }
+	    
 	}
